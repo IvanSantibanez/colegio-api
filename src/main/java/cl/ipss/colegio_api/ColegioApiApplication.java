2eaaf22b0f2ac4ea96ebdffcdd0ff7c764d80c42
Iván Santibáñez <ivan.t.asc@gmail.com>
2025-12-06 17:28:03 -0300

    first commit

---
+++ src/main/java/cl/ipss/colegio_api/ColegioApiApplication.java
@@ -0,0 +1,13 @@
+package cl.ipss.colegio_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColegioApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ColegioApiApplication.class, args);
+	}
+
+}
